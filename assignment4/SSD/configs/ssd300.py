dd7ae7e56f4b5c8ee94c397461f46ba0e2542adf
KristofferSkare <kristoffer.skare@live.com>
2022-03-24 12:13:01 +0100

    Try this

--- assignment4/SSD/configs/ssd300.py
+++ assignment4/SSD/configs/ssd300.py
@@ -41,12 +41,12 @@ backbone = L(backbones.BasicModel)(
     output_feature_sizes="${anchors.feature_sizes}"
 )
 
-#loss_objective = L(SSDMultiboxLoss)(anchors="${anchors}")
+loss_objective = SSDMultiboxLoss#L(SSDMultiboxLoss)(anchors="${anchors}")
 
 model = L(SSD300)(
     feature_extractor=backbone,
     anchors=anchors,
-    loss_objective=SSDMultiboxLoss,
+    loss_objective=loss_objective,
     num_classes=10 + 1  # Add 1 for background
 )
 
