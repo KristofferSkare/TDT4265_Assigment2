f638be41fa9f4c2da3e8161de9c27d66cd34d2a9
Celine Uyen Tram Tran <cutran@oppdal.idi.ntnu.no>
2022-03-25 10:41:49 +0100

    Text in notebook for task 4

--- assignment4/SSD/ssd/modeling/backbones/basic.py
+++ assignment4/SSD/ssd/modeling/backbones/basic.py
@@ -22,7 +22,6 @@ class BasicModel(torch.nn.Module):
         self.out_channels = output_channels
         self.output_feature_shape = output_feature_sizes
 
-        dropout_p = 0.5
         blocks = [
             [
                 nn.Conv2d(in_channels=image_channels, out_channels=32, kernel_size=3, stride=1, padding=1),
@@ -37,57 +36,41 @@ class BasicModel(torch.nn.Module):
                 nn.ReLU(),
                 nn.BatchNorm2d(64),
                 nn.Conv2d(in_channels=64, out_channels=output_channels[0], kernel_size=3, stride=2, padding=1),
-                nn.Dropout2d(p=dropout_p),
                 nn.ReLU(),
                 nn.BatchNorm2d(output_channels[0]),
             ],
             [
                 nn.Conv2d(in_channels=output_channels[0], out_channels=128, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(),
-                nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
                 nn.Conv2d(in_channels=128, out_channels=output_channels[1], kernel_size=3, stride=2, padding=1),
-                nn.Dropout2d(p=dropout_p),
                 nn.ReLU(),
                 nn.BatchNorm2d(output_channels[1]),
             ],
             [
-                nn.Conv2d(in_channels=output_channels[1], out_channels=128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+                nn.Conv2d(in_channels=output_channels[1], out_channels=256, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(),
                 nn.Conv2d(in_channels=256, out_channels=output_channels[2], kernel_size=3, stride=2, padding=1),
-                nn.Dropout2d(p=dropout_p),
                 nn.ReLU(),
                 nn.BatchNorm2d(output_channels[2]),
             ],
             [
                 nn.Conv2d(in_channels=output_channels[2], out_channels=128, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(),
-                nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
                 nn.Conv2d(in_channels=128, out_channels=output_channels[3], kernel_size=3, stride=2, padding=1),
-                nn.Dropout2d(p=dropout_p),
                 nn.ReLU(),
                 nn.BatchNorm2d(output_channels[3]),
             ],
             [
                 nn.Conv2d(in_channels=output_channels[3], out_channels=128, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(),
-                nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
                 nn.Conv2d(in_channels=128, out_channels=output_channels[4], kernel_size=3, stride=2, padding=1),
-                nn.Dropout2d(p=dropout_p),
                 nn.ReLU(),
                 nn.BatchNorm2d(output_channels[4]),
             ],
             [
                 nn.Conv2d(in_channels=output_channels[4], out_channels=128, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(),
-                nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
                 nn.Conv2d(in_channels=128, out_channels=output_channels[5], kernel_size=3, stride=1, padding=0),
-                nn.Dropout2d(p=dropout_p),
                 nn.ReLU(),
                 nn.BatchNorm2d(output_channels[5]),
             ],
